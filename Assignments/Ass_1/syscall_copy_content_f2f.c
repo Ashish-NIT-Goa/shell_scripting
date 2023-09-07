8eab6b89008d0f237fb1fa37fd0576fa1420f924
Ashish-NIT-Goa <ashishchauhan123ch@nitgoa.ac.in>
2023-09-07 21:51:08 +0000

    Add my assignment

---
+++ Assignments/Ass_1/syscall_copy_content_f2f.c
@@ -0,0 +1,35 @@
+// system call program to copy content of file 1 to file 2
+
+#include<stdio.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<string.h>
+
+void copy_content(char *f1,char *f2) {
+    int fd1,fd2,nread;
+    char buf[1024];
+    fd1=open(f1,O_RDONLY);
+    fd2=open(f2,O_WRONLY);
+    if(fd1==-1 || fd2==-1) {
+        perror(f1);
+        perror(f2);
+        return;
+    }
+    nread=read(fd1,buf,sizeof(buf));
+    while(nread > 0) {
+        write(fd2,buf,nread);
+        nread=read(fd1,buf,sizeof(buf));
+    }
+    printf("Successfully copied contents of file-1 to file-2\n");
+    close(fd2);
+    close(fd1);
+}
+
+int main( int argc,char **argv) {
+    int argno=0;
+    char *file1;
+    char *file2;
+    file1=argv[1];
+    file2=argv[2];
+    copy_content(file1,file2);
+}
